161a4afc6db19cea38f159ae93c073817cd5750f
shanni6 <240686886@qq.com>
2023-12-10 02:06:08 -0800

    create delete comment

--- routes/videos.js
+++ routes/videos.js
@@ -27,10 +27,20 @@ router.get("/:id", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+    let data = fs.readFileSync("data/videos.json", "utf-8");
+    data = JSON.parse(data);
+
     const video = req.body;
     video.id = uuidv4();
     video.timestamp = Date.now();
-    videos.push(video);
+    video.likes = "0";
+    video.views = "0";
+    video.comments = [];
+    video.duration = "0:00";
+    video.image = "upload.jpg";
+    
+    data.push(video);
+    fs.writeFileSync("data/videos.json", JSON.stringify(data));
     res.json(video);
 });
 
@@ -40,6 +50,7 @@ router.post("/:id/comments", (req, res) => {
     newComment.id = uuidv4();
     newComment.timestamp = Date.now();
     newComment.likes = 0;
+    
     let data = fs.readFileSync("data/videos.json", "utf-8");
     data = JSON.parse(data);
 
@@ -51,6 +62,25 @@ router.post("/:id/comments", (req, res) => {
     fs.writeFileSync("data/videos.json", JSON.stringify(data));
 
     res.json(newComment);
+});
+
+router.delete("/:videoId/comments/:commentId", (req, res) => {
+    const videoId = req.params.videoId;
+    const commentId = req.params.commentId;
+    let data = fs.readFileSync("data/videos.json", "utf-8");
+    data = JSON.parse(data);
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id === videoId) {
+            for (let j = 0; j < data[i].comments.length; j++) {
+                if (data[i].comments[j].id === commentId) {
+                    const comment = data[i].comments[j];
+                    data[i].comments.splice(j, 1);
+                    fs.writeFileSync("data/videos.json", JSON.stringify(data));
+                    res.json(comment);
+                }
+            }
+        }
+    }
 });
 
 module.exports = router;
